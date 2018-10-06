ff9395b9c582aa799530cc3513938ea4c5693c58
HoverEpic <pcsrvadm@gmail.com>
2018-10-06 13:43:11 +0200

    Update setup client script

--- setup_client.sh
+++ setup_client.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-//Vino script, enable remote view and control
+# Vino script, enable remote view and control
 
-// enable vino
-gsettings set org.gnome.Vino enable true
+# Vino password, if changed, please run this script again on all servers.
+# -> VNC Enc. Password
+VINO_PASSWORD='65scv52n'
 
-// set password
-gsettings set org.gnome.Vino vnc-password 65scv52n
+# enable vino
+gsettings set org.gnome.Vino enabled true
 
-// no notify on connect
+# set password
+gsettings set org.gnome.Vino vnc-password $VINO_PASSWORD
+
+# no notify on connect
 gsettings set org.gnome.Vino notify-on-connect false
 
-// not ask for connection
+# not ask for connection
 gsettings set org.gnome.Vino prompt-enabled false
 
-// disable protocol encryption
+# not display icon
+gsettings set org.gnome.Vino icon-visibility never
+
+# disable protocol encryption
 gsettings set org.gnome.Vino require-encryption false
